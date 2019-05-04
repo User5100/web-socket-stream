eaaf22f6970e3e277caf20c5122777831241a334
User5100 <maot.tm@gmail.com>
2019-05-04 16:41:21 +0100

    Transform stream to upper case

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const fs = require("fs")
+const through = require("through2")
 
 fs
     .createReadStream("./text.txt")
+    .pipe(through(toUpper))
     .pipe(process.stdout)
+
+function toUpper(buf, enc, next) {
+    next(null, buf.toString().toUpperCase())
+}
 
